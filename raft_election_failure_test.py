9ef4c367ee5a7626cc3f2ace1f0f25d69e31561d
Nikita Borisov <git@nikita.ca>
2021-11-09 20:43:14 -0600

    use finally clause to stop processes

--- raft_election_failure_test.py
+++ raft_election_failure_test.py
@@ -15,45 +15,49 @@ async def main():
     group = raft_test.RaftGroup(n)
     await alog.init(INFO)
 
-    await group.start(command)
+    try: 
+        await group.start(command)
 
-    await alog.log(INFO, "Waiting for a leader to be elected")
-    await group.wait_for_normal_op(n, 0)
+        await alog.log(INFO, "Waiting for a leader to be elected")
+        await group.wait_for_normal_op(n, 0)
 
-    if len(group.leaders) > 1:
-        await alog.log(ERROR, "### Error!  more than 1 term with a leader despite no failures!")
-        await alog.log(ERROR, f"leaders: {group.leaders}")
-        return
+        if len(group.leaders) > 1:
+            await alog.log(ERROR, "### Error!  more than 1 term with a leader despite no failures!")
+            await alog.log(ERROR, f"leaders: {group.leaders}")
+            return
 
 
-    term, leader = max(group.leaders.items())
-    await alog.log(INFO, f"# Successfully elected {leader} for term {term}")
+        term, leader = max(group.leaders.items())
+        await alog.log(INFO, f"# Successfully elected {leader} for term {term}")
 
-    await alog.log(INFO, f"# Stopping leader {leader}, waiting for next one to be elected")
+        await alog.log(INFO, f"# Stopping leader {leader}, waiting for next one to be elected")
 
-    group.stop(leader)
+        group.stop(leader)
 
-    await group.wait_for_normal_op(n-1, term)
+        await group.wait_for_normal_op(n-1, term)
 
-    if len(group.leaders) > 2:
-        await alog.log(ERROR, "### Error!  more than 2 terms with a leader!")
-        return
+        if len(group.leaders) > 2:
+            await alog.log(ERROR, "### Error!  more than 2 terms with a leader!")
+            await alog.log(ERROR, f"leaders: {group.leaders}")
+            return
 
-    term2, leader2 = max(group.leaders.items())
+        term2, leader2 = max(group.leaders.items())
 
-    if leader2 == leader:
-        await alog.log(ERROR, "### Error! new leader cannot be stopped process!")
-        return
+        if leader2 == leader:
+            await alog.log(ERROR, "### Error! new leader cannot be stopped process!")
+            return
 
-    await alog.log(INFO, f"# Successfully elected {leader2} for term {term2}")
-    await alog.log(INFO, "### Election after failure test passed!")
+        await alog.log(INFO, f"# Successfully elected {leader2} for term {term2}")
+        await alog.log(INFO, "### Election after failure test passed!")
+        await alog.log(INFO, f"# Sent {group.network.message_count} messages, {group.network.byte_count} bytes")
 
-    group.stop_all()
+    finally:
+        group.stop_all()
+        await asyncio.sleep(1)
 
-    await alog.log(INFO, f"# Sent {group.network.message_count} messages, {group.network.byte_count} bytes")
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
+    asyncio.run(main())
 
 
     
--- raft_election_test.py
+++ raft_election_test.py
@@ -12,30 +12,34 @@ async def main():
         command = sys.argv[2:]
     else:
         command = ["./raft"]
-    group = raft_test.RaftGroup(n)
+    
     await alog.init(INFO)
+    group = raft_test.RaftGroup(n)
 
-    await group.start(command)
+    try:
+        await group.start(command)
 
-    await alog.log(INFO, f"Waiting for a leader to be elected")
-    await group.wait_for_normal_op(n, 0)
+        await alog.log(INFO, f"Waiting for a leader to be elected")
+        await group.wait_for_normal_op(n, 0)
 
-    if len(group.leaders) > 1:
-        await alog.log(ERROR, "### Error!  more than 1 term with a leader despite no failures!")
-        await asyncio.sleep(5)
-        return
+        if len(group.leaders) > 1:
+            await alog.log(ERROR, "### Error!  more than 1 term with a leader despite no failures!")
+            await alog.log(ERROR, f"Leaders: {group.leaders}")
+            return
 
-    term, leader = max(group.leaders.items())
+        term, leader = max(group.leaders.items())
 
-    await alog.log(INFO, f"# Successfully elected {leader} for term {term}")
-    await alog.log(INFO, f"### Election test passed")
+        await alog.log(INFO, f"# Successfully elected {leader} for term {term}")
+        await alog.log(INFO, f"### Election test passed")
 
-    group.stop_all()
+        await alog.log(INFO, f"# Sent {group.network.message_count} messages, {group.network.byte_count} bytes")
+    finally:
+        group.stop_all()
+        await asyncio.sleep(1)
 
-    await alog.log(INFO, f"# Sent {group.network.message_count} messages, {group.network.byte_count} bytes")
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
+    asyncio.run(main())
     
     
         
