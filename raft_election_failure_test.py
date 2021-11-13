32b3eb7982f124b33b2e5f04af173e25e8f6db0e
Nikita Borisov <git@nikita.ca>
2021-11-12 20:33:07 -0600

    refactored the tests

--- raft_election_failure_test.py
+++ raft_election_failure_test.py
@@ -3,73 +3,39 @@ import sys
 import raft_test
 import alog
 from logging import INFO, ERROR
+import raft_election_test
 
 
-async def main():
-    n = int(sys.argv[1])
-    if len(sys.argv) > 2:
-        command = sys.argv[2:]
-    else:
-        command = ["./raft"]
 
-    group = raft_test.RaftGroup(n)
-    await alog.init(INFO)
+async def main(n, group):
+    term, leader = await raft_election_test.elect_leader(n, group)
 
-    try: 
-        await group.start(command)
+    await alog.log(INFO, f"# Stopping leader {leader}, waiting for next one to be elected")
 
-        await alog.log(INFO, f"Waiting for a leader to be elected")
-        def reached_normal_op(group):
-            if not group.normal_op: # no leaders yet
-                return False
-            _, nodes = max(group.normal_op.items())
-            if len(nodes) == n:
-                return True
-            else:
-                return False
-        await group.wait_predicate(reached_normal_op)
+    group.stop(leader)
 
-        if len(group.leaders) > 1:
-            await alog.log(ERROR, "### Error!  more than 1 term with a leader despite no failures!")
-            await alog.log(ERROR, f"leaders: {group.leaders}")
-            return
+    def next_term(group):
+        term2, nodes = max(group.normal_op.items())
+        return term2 > term and len(nodes) == n-1
 
+    await group.wait_predicate(next_term)
 
-        term, leader = max(group.leaders.items())
-        await alog.log(INFO, f"# Successfully elected {leader} for term {term}")
+    if len(group.leaders) > 2:
+        await alog.log(ERROR, "### Error!  more than 2 terms with a leader!")
+        await alog.log(ERROR, f"leaders: {group.leaders}")
+        return
 
-        await alog.log(INFO, f"# Stopping leader {leader}, waiting for next one to be elected")
+    term2, leader2 = max(group.leaders.items())
 
-        group.stop(leader)
+    if leader2 == leader:
+        await alog.log(ERROR, "### Error! new leader cannot be stopped process!")
+        return
 
-        def next_term(group):
-            term2, nodes = max(group.normal_op.items())
-            return term2 > term and len(nodes) == n-1
-
-        await group.wait_predicate(next_term)
-
-        if len(group.leaders) > 2:
-            await alog.log(ERROR, "### Error!  more than 2 terms with a leader!")
-            await alog.log(ERROR, f"leaders: {group.leaders}")
-            return
-
-        term2, leader2 = max(group.leaders.items())
-
-        if leader2 == leader:
-            await alog.log(ERROR, "### Error! new leader cannot be stopped process!")
-            return
-
-        await alog.log(INFO, f"# Successfully elected {leader2} for term {term2}")
-        await alog.log(INFO, "### Election after failure test passed!")
-        await alog.log(INFO, f"# Sent {group.network.message_count} messages, {group.network.byte_count} bytes")
-
-    finally:
-        group.stop_all()
-        await asyncio.sleep(1)
-
+    await alog.log(INFO, f"# Successfully elected {leader2} for term {term2}")
+    await alog.log(INFO, "### Election after failure test passed!")
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(raft_test.run_test(main))
 
 
     
--- raft_partition_test.py
+++ raft_partition_test.py
@@ -5,81 +5,49 @@ from collections import defaultdict
 import alog
 from logging import INFO, CRITICAL, DEBUG, ERROR
 import raft_test
+import raft_election_test
 
-async def main():
-    n = int(sys.argv[1])
-    if len(sys.argv) > 2:
-        command = sys.argv[2:]
-    else:
-        command = ["./raft"]
-    group = raft_test.RaftGroup(n)
-    await alog.init(INFO)
+async def main(n, group):
+    term, leader = await raft_election_test.elect_leader(n, group)
 
-    try: 
-        await group.start(command)
+    await alog.log(INFO, f"# Partitioning off leader {leader}, waiting for next one to be elected")
+    group.network.set_partition([leader], [str(p) for p in range(n) if str(p) != leader])
 
-        await alog.log(INFO, f"Waiting for a leader to be elected")
-        def reached_normal_op(group):
-            if not group.normal_op: # no leaders yet
-                return False
-            _, nodes = max(group.normal_op.items())
-            if len(nodes) == n:
-                return True
-            else:
-                return False
-        await group.wait_predicate(reached_normal_op)
+    def next_term(group):
+        term2, nodes = max(group.normal_op.items())
+        return term2 > term and len(nodes) == n-1
 
-        if len(group.leaders) > 1:
-            await alog.log(ERROR, "### Error!  more than 1 term with a leader despite no failures!")
-            return
+    await group.wait_predicate(next_term)
 
-        term, leader = max(group.leaders.items())
+    if len(group.leaders) > 2:
+        await alog.log(ERROR, "### Error!  more than 2 terms with a leader!")
+        return
 
-        await alog.log(INFO, f"# Successfully elected {leader} for term {term}")
+    term2, leader2 = max(group.leaders.items())
 
-        await alog.log(INFO, f"# Partitioning off leader {leader}, waiting for next one to be elected")
-        group.network.set_partition([leader], [str(p) for p in range(n) if str(p) != leader])
+    if leader2 == leader:
+        await alog.log(ERROR, "### Error! Partitioned leader elected!")
+        return
 
-        def next_term(group):
-            term2, nodes = max(group.normal_op.items())
-            return term2 > term and len(nodes) == n-1
+    await alog.log(INFO, f"# Successfully elected {leader2} for term {term2}")
+    await alog.log(INFO, f"# Repairing partition, waiting for old leader ({leader}) to catch up")
 
-        await group.wait_predicate(next_term)
+    group.network.repair_partition()
 
-        if len(group.leaders) > 2:
-            await alog.log(ERROR, "### Error!  more than 2 terms with a leader!")
-            return
+    def full_group(group):
+        _, nodes = max(group.normal_op.items())
+        return len(nodes) == n
 
-        term2, leader2 = max(group.leaders.items())
+    await group.wait_predicate(full_group)
 
-        if leader2 == leader:
-            await alog.log(ERROR, "### Error! Partitioned leader elected!")
-            return
+    if len(group.leaders) > 2:
+        await alog.log(ERROR, "### Error! Repairing partition should not result in a new term")
+        return 
 
-        await alog.log(INFO, f"# Successfully elected {leader2} for term {term2}")
-        await alog.log(INFO, f"# Repairing partition, waiting for old leader ({leader}) to catch up")
-
-        group.network.repair_partition()
-
-        def full_group(group):
-            _, nodes = max(group.normal_op.items())
-            return len(nodes) == n
-
-        await group.wait_predicate(full_group)
-
-        if len(group.leaders) > 2:
-            await alog.log(ERROR, "### Error! Repairing partition should not result in a new term")
-            return 
-
-        await alog.log(INFO, "### Partition test passed!")
-        await alog.log(INFO, f"# Sent {group.network.message_count} messages, {group.network.byte_count} bytes")
-    finally:
-        group.stop_all()
-        await asyncio.sleep(1)
-
+    await alog.log(INFO, "### Partition test passed!")
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(raft_test.run_test(main))
     
     
         
